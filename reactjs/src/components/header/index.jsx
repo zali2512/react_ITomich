f8763ee95cb1b933a58f9146176dd4bc828ec375
zali2512 <zalinaarsoeva@gmail.com>
2022-08-03 16:29:33 +0300

    Added HW15 new tag

--- reactjs/src/components/header/index.jsx
+++ reactjs/src/components/header/index.jsx
@@ -1,37 +1,35 @@
-
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import "./style.css";
+import './style.css'
 
 function DataFetch() {
-    const [post, setPost] = useState({})
-    const [id,setId] = useState(1)
+  const[posts, SetPosts] = useState([])
+  const[userId, setUserId] = useState(1)
 
-    useEffect(() => {
-        axios
-            .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then(res => {
-                console.log(res)
-                setPost(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [id])
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    .then(res => {
+      console.log(res)
+      SetPosts(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [userId])
 
-    return (
-        <div className="field">
-            <label htmlFor="userId">Write down the userId for necessary title and body: </label>
-            <input type="text" value={id} onChange={changer => setId(changer.target.value)}  />
-            <div>
-                <p>{post.title}</p>
-                <p>{post.body}</p> </div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <label htmlFor="userId">Write down the userId for necessary title and body: </label>
+      <p><input type="text" value={userId} onChange={e => setUserId(e.target.value) } /></p>
+      <ul>
+        {
+          posts.map(post => <p key={post.userId}>post title: {post.title}</p>)
+        }
+      </ul>
+      
+    </div>
+  )
+
 }
 
-
-
 export default DataFetch;
-
